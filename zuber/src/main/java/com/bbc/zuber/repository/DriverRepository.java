7f926fcec3ed6dffc9750ec7a18411197fbac923
Krystian Bogacz <pefekbiznes@gmail.com>
2023-11-28 10:03:14 +0100

    DriverRepository update

--- zuber/src/main/java/com/bbc/zuber/repository/DriverRepository.java
+++ zuber/src/main/java/com/bbc/zuber/repository/DriverRepository.java
@@ -2,6 +2,8 @@ package com.bbc.zuber.repository;
 
 import com.bbc.zuber.model.driver.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
 }
